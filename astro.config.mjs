557a2d6634840113aedca6f2aba8c1283f8dcfc6
Jaehwan <dialektike@gmail.com>
2023-01-23 16:35:08 +0900

    설정 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+// export default defineConfig({
+// 	integrations: [
+// 		// Enable Preact to support Preact JSX components.
+// 		preact(),
+// 		// Enable React for the Algolia search component.
+// 		react(),
+// 	],
+// 	site: `https://astro.build`,
+// });
+
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
-});
+	site: 'https://tossicat.github.io',
+	base: '/my-repo',
+  });
